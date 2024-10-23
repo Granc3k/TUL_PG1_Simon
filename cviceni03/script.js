7eff80acd0c369327ac2af1f5a28ea8a7fc7a4b8
Martin Granc3k Simon <mart.simon17@gmail.com>
2024-10-23 18:47:59 +0200

    cv03  done

--- cviceni03/script.js
+++ cviceni03/script.js
@@ -1,88 +1,165 @@
+// Bayerovy matice pro dithering
+const matrices = {
+  "2x2": [
+    [0, 2],
+    [3, 1],
+  ],
+  "4x4": [
+    [0, 8, 2, 10],
+    [12, 4, 14, 6],
+    [3, 11, 1, 9],
+    [15, 7, 13, 5],
+  ],
+  "8x8": [
+    [0, 32, 8, 40, 2, 34, 10, 42],
+    [48, 16, 56, 24, 50, 18, 58, 26],
+    [12, 44, 4, 36, 14, 46, 6, 38],
+    [60, 28, 52, 20, 62, 30, 54, 22],
+    [3, 35, 11, 43, 1, 33, 9, 41],
+    [51, 19, 59, 27, 49, 17, 57, 25],
+    [15, 47, 7, 39, 13, 45, 5, 37],
+    [63, 31, 55, 23, 61, 29, 53, 21],
+  ],
+};
+
+document.getElementById("kValue").addEventListener("input", function () {
+  document.getElementById("kDisplay").innerText = this.value;
+});
+
 // Callback function called, when file is "opened"
 function handleFileSelect(item) {
-	var files = item.files;
+  var files = item.files;
 
-	console.log(files);
+  for (var i = 0; i < files.length; i++) {
+    if (!files[i].type.match("image.*")) {
+      continue;
+    }
 
-	for (var i = 0; i < files.length; i++) {
-		console.log(files[i], files[i].name, files[i].size, files[i].type);
+    var reader = new FileReader();
 
-		// Only process image files.
-		if (!files[i].type.match('image.*')) {
-			continue;
-		}
+    reader.onload = (function (file) {
+      return function (evt) {
+        var srcImg = new Image();
+        srcImg.src = evt.target.result;
 
-		var reader = new FileReader();
+        srcImg.onload = function () {
+          var srcCanvas = document.getElementById("src");
+          var srcContext = srcCanvas.getContext("2d");
 
-		// Closure for loading image to memory
-		reader.onload = (function(file) {
-			return function(evt) {
+          // zmeneni velikosti canvasu na velikost obrazku
+          srcCanvas.height = srcImg.height;
+          srcCanvas.width = srcImg.width;
 
-				var srcImg = new Image();
-				srcImg.src = evt.target.result;
+          // print obrazku na canvas
+          srcContext.drawImage(srcImg, 0, 0);
 
-				srcImg.onload = function() {
-					var srcCanvas = document.getElementById("src");
-					var srcContext = srcCanvas.getContext("2d");
+          // nastaveni ostatnich canvasu
+          for (var i = 1; i <= 3; i++) {
+            var outputCanvas = document.getElementById("outputCanvas" + i);
+            outputCanvas.height = srcImg.height;
+            outputCanvas.width = srcImg.width;
+          }
 
-					// Change size of canvas
-					srcCanvas.height = srcImg.height;
-					srcCanvas.width = srcImg.width;
+          var convertButton = document.getElementById("convert");
+          convertButton.disabled = false;
+          convertButton.addEventListener("click", convertImage, false);
+        };
+      };
+    })(files[i]);
 
-					srcContext.drawImage(srcImg, 0, 0);
+    reader.readAsDataURL(files[i]);
 
-					var convertButton = document.getElementById("convert");
-					// Enabled button
-					convertButton.disabled = false;
-					// Add callback
-					convertButton.addEventListener('click', convertImage, false);
-				}
-			}
-		})(files[i]);
-
-		reader.readAsDataURL(files[i]);
-
-		break;
-	}
+    break;
+  }
 }
 
-
-// Callback function called, when clicked at Convert button
+// Callback pro konverzi obrazu pri kliknuti na tlacitko Convert
 function convertImage() {
-	var srcCanvas = document.getElementById("src");
-	var srcContext = srcCanvas.getContext("2d");
-	var canvasHeight = srcCanvas.height;
-	var canvasWidth = srcCanvas.width;
+  var srcCanvas = document.getElementById("src");
+  var srcContext = srcCanvas.getContext("2d");
+  var canvasHeight = srcCanvas.height;
+  var canvasWidth = srcCanvas.width;
 
-	var srcImageData = srcContext.getImageData(0, 0, canvasWidth, canvasHeight);
+  // load puvodnich obrazovych dat
+  var srcImageData = srcContext.getImageData(0, 0, canvasWidth, canvasHeight);
 
-	convertImageData(srcImageData);
+  // getne aktualni nastaveni z UI
+  var matrixSelect = document.getElementById("matrixSelect").value;
+  var kValue = parseFloat(document.getElementById("kValue").value);
 
-	srcContext.putImageData(srcImageData, 0, 0);
+  // vybere choosnutou matici a ostatni pro dynamicky print na canvasy
+  var selectedMatrix = matrices[matrixSelect];
+  var otherMatrices = Object.keys(matrices).filter(
+    (key) => key !== matrixSelect
+  );
+
+  // check, ze jsou vsechny matice v poradku
+  var matricesOrder = [matrixSelect, ...otherMatrices];
+
+  // aktualizace popisku
+  for (var i = 1; i <= 3; i++) {
+    var matrixKey = matricesOrder[i - 1];
+    var label = document.getElementById("label" + i);
+    label.innerText = "Výstup pro matici " + matrixKey;
+  }
+
+  // vytvoreni vystupu pro kazdou matici
+  for (var i = 1; i <= 3; i++) {
+    var outputCanvas = document.getElementById("outputCanvas" + i);
+    var outputContext = outputCanvas.getContext("2d");
+
+    // vytvoreni nove kopie obrazovych dat pro kazdou matici
+    var outputImageData = new ImageData(
+      new Uint8ClampedArray(srcImageData.data),
+      canvasWidth,
+      canvasHeight
+    );
+
+    // getne aktualni matici na zaklade indexu
+    var matrixKey = matricesOrder[i - 1];
+    var matrix = matrices[matrixKey];
+    var matrixSize = matrix.length;
+
+    // provedeni konverze na zaklade aktualni matice a hodnoty k
+    convertImageData(outputImageData, matrixSize, matrix, kValue);
+
+    // print na canvas
+    outputContext.putImageData(outputImageData, 0, 0);
+  }
 }
 
+// funkce pro konverzi obrazku do grayscalu
+function convertImageData(imgData, matrixSize, matrix, k) {
+  var rawData = imgData.data;
 
-// Function for converting raw data of image
-function convertImageData(imgData) {
-	var rawData = imgData.data;
+  for (var y = 0; y < imgData.height; y++) {
+    for (var x = 0; x < imgData.width; x++) {
+      var pixelIndex = (imgData.width * y + x) * 4;
+      var red = rawData[pixelIndex + 0];
+      var green = rawData[pixelIndex + 1];
+      var blue = rawData[pixelIndex + 2];
+      var alpha = rawData[pixelIndex + 3];
 
-	// Go through the image using x,y coordinates
-	var pixelIndex, red, green, blue, alpha;
-	for(var y = 0; y < imgData.height; y++) {
-		for(var x = 0; x < imgData.width; x++) {
-			pixelIndex = ( (imgData.width * y) + x) * 4
-			red   = rawData[pixelIndex + 0];
-			green = rawData[pixelIndex + 1];
-			blue  = rawData[pixelIndex + 2];
-			alpha = rawData[pixelIndex + 3];
+      // konverze BT.601
+      var gray = 0.299 * red + 0.587 * green + 0.114 * blue;
 
-			// Do magic at this place :-)
+      // normalizace matice do rozsahu 0-255 a aplikace hodnoty k z UI
+      var threshold =
+        (matrix[y % matrixSize][x % matrixSize] / (matrixSize * matrixSize)) *
+        255 *
+        k;
 
-			rawData[pixelIndex + 0] = 255 - red;
-			rawData[pixelIndex + 1] = 255 - green;
-			rawData[pixelIndex + 2] = 255 - blue;
-			rawData[pixelIndex + 3] = alpha;
-		}
-	}	
+      // Aplikace prahovani (thresholding)
+      if (gray > threshold) {
+        gray = 255;
+      } else {
+        gray = 0;
+      }
+
+      rawData[pixelIndex + 0] = gray; // red
+      rawData[pixelIndex + 1] = gray; // green
+      rawData[pixelIndex + 2] = gray; // blue
+      rawData[pixelIndex + 3] = alpha; // alpha (zustane stejna)
+    }
+  }
 }
-
